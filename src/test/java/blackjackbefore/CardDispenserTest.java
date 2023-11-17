2812b16f4b72b081272cace4cd5e2d4c0c580525
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-11-17 23:44:13 +0900

    [feat] 카드 지급

---
+++ src/test/java/blackjackbefore/CardDispenserTest.java
@@ -0,0 +1,13 @@
+package blackjackbefore;
+
+import blackjackbefore.domains.CardDispenser;
+import org.junit.jupiter.api.Test;
+
+public class CardDispenserTest {
+    @Test
+    void 랜덤카드지급테스트() {
+        CardDispenser cardDispenser = new CardDispenser();
+        System.out.println(cardDispenser.getRandomOneCard());
+
+    }
+}
