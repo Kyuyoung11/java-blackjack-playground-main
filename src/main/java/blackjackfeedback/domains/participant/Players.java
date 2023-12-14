b5716029702a4669b14cebc41e0ed42c952abc94
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-12-15 00:36:32 +0900

    [feat] player 일급컬렉션

---
+++ src/main/java/blackjackfeedback/domains/participant/Players.java
@@ -0,0 +1,11 @@
+package blackjackfeedback.domains.participant;
+
+import java.util.List;
+
+public class Players {
+    List<Player> players;
+
+    public Players(List<Player> players) {
+        this.players = players;
+    }
+}
