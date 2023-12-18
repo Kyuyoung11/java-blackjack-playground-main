4e53b71486815af2f9554dfb0866e91c7b38d69b
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-12-18 23:36:21 +0900

    [fix] implements 메소드 수정

--- src/main/java/blackjackfeedback/domains/state/Started.java
+++ src/main/java/blackjackfeedback/domains/state/Started.java
@@ -2,33 +2,12 @@ package blackjackfeedback.domains.state;
 
 
 import blackjackfeedback.domains.card.Cards;
-import blackjackfeedback.domains.card.PlayingCard;
 
 public abstract class Started implements State{
     private Cards cards;
 
     @Override
     public Cards cards() {
-        return cards;
-    }
-
-    @Override
-    public State draw(PlayingCard card) {
-        return null;
-    }
-
-    @Override
-    public State stay() {
-        return null;
-    }
-
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
-
-    @Override
-    public double profit(double rate) {
-        return 0;
+        return this.cards;
     }
 }
